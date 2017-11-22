01530332c59b551a65f38fd5bfa116504167134e
Fabian Woltermann <fwoltermann@gmail.com>
2017-11-22 16:22:09 +0100

    doodle

--- GinClientLibrary/GinRepository.cs
+++ GinClientLibrary/GinRepository.cs
@@ -16,6 +16,7 @@ namespace GinClient
         public string Name { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public DirectoryInfo PhysicalDirectory { get; set; }
 
         public enum FileStatus
         {
