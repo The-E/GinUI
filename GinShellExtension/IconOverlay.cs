b70bfee378e5eeb4ffab9214cfd116246c9dee65
Fabian Woltermann <fwoltermann@gmail.com>
2017-12-10 13:41:20 +0100

    Base implementation of IconOverlay

--- GinShellExtension/IconOverlay.cs
+++ GinShellExtension/IconOverlay.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
 using SharpShell.Interop;
 using SharpShell.SharpIconOverlayHandler;
 
 namespace GinShellExtension
 {
+    [ComVisible(true)]
     public class IconOverlay : SharpIconOverlayHandler
     {
         protected override int GetPriority()
