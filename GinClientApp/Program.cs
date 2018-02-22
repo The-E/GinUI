93cb8cd5d4dadef145b28c832189690a68a61986
Fabian Woltermann <fwoltermann@gmail.com>
2018-02-22 18:32:59 +0100

    Only allow one instance of the client app to exist

--- GinClientApp/Program.cs
+++ GinClientApp/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace GinClientApp
 {
     internal static class Program
     {
+        static Mutex mutex = new Mutex(true, "{AC8AB48D-C289-445D-B1EB-ABCFF24443ED}");
+
         /// <summary>
         ///     The main entry point for the application.
         /// </summary>
         [STAThread]
         private static void Main()
         {
+            if (!mutex.WaitOne(TimeSpan.Zero, true)) return;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GinApplicationContext());
